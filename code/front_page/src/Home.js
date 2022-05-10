fdc33ec975f1d7ac4e17273123d920a433a72268
jiangsijia <1537523736@qq.com>
2022-05-10 18:08:54 +0800

    home

--- code/front_page/src/Home.js
+++ code/front_page/src/Home.js
@@ -4,7 +4,10 @@ import HomeContent from './HomeContent'
 import './Home.css'
 
 const Home = () => {
-    localStorage.setItem('tip',false);
+    // localStorage.setItem('tip',false);
+    localStorage.setItem('dis2D',JSON.stringify({display:'none'}))
+    localStorage.setItem('dis3D',JSON.stringify({display:'none'}))
+    localStorage.setItem('disStart',JSON.stringify({display:'flex'}))
     return (
         <div className="home">
             <HomeHeader></HomeHeader>
